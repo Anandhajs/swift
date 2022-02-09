0ab61738126652850f59b0ce5bf40a38b1a342cb
anand <anand@sparkouttech.com>
2022-02-09 10:39:02 +0530

    swift basics

--- clos.playground/Contents.swift
+++ clos.playground/Contents.swift
@@ -1,12 +1,46 @@
 import UIKit
 
-enum student
-{
-     add()
-        let  b: Int = 10
-    
-         //return b + b
-    
+/*v ar name = [ " Mom", "Dad", "Wife", "Friend" ]
+    var words = [ "I Miss You", "I Hate You", "I Sing You" ]
+    for Name in name{
+    print("\n\n" + Name)
+    print()
+    for Words in words{
+        print("\(Words)")
+    }
+
+
+    print()
+    } */
+
+func number(value: Int) -> Int{
+    var num = 0
+    var array: [Int] = []
+    var nNumber = value
+    while nNumber > 10
+    {
+        num = nNumber % 10
+        array.append(num)
+        nNumber /= 10
+    }
+    array.append(nNumber)
+    let rev = reverse(aNumber: array)
+    let sum = add(rev: rev)
+    return sum
 }
-var value = student.self
-value = .add()
+func reverse(aNumber: [Int]) -> [Int]
+{
+    return aNumber.reversed()
+}
+func add(rev: [Int]) -> Int
+{
+    var sum = 0
+    for i in 1..<rev.count
+    {
+        sum += rev[i]
+    }
+    return sum
+}
+number(value: 546312)
+
+
